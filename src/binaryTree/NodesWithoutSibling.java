6366802ccd4107df9cbc22083634bd1cd2965b89
Nijin P S <nijingithub@gmail.com>
2022-04-03 13:19:59 +0530

    Add code to print nodes not having siblings

---
+++ src/binaryTree/NodesWithoutSibling.java
@@ -0,0 +1,23 @@
+/*
+For a given Binary Tree of type integer, print all the nodes without any siblings.
+ */
+
+package binaryTree;
+
+public class NodesWithoutSibling {
+
+    public static void printNodesWithoutSibling(BinaryTreeNode<Integer> root) {
+        //Your code goes here
+        if(root==null)
+            return;
+
+        if(root.left==null && root.right!=null)
+            System.out.print(root.right.data+" ");
+
+        if(root.left!=null && root.right==null)
+            System.out.print(root.left.data+" ");
+
+        printNodesWithoutSibling(root.left);
+        printNodesWithoutSibling(root.right);
+    }
+}
