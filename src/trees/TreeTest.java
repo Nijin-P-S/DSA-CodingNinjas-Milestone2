876956677c90ea60d2de6f8bad756598d36a6595
Nijin P S <nijingithub@gmail.com>
2022-04-11 20:39:57 +0530

    Add code to get data for tree level wise

--- src/trees/TreeTest.java
+++ src/trees/TreeTest.java
@@ -1,8 +1,44 @@
 package trees;
 
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 
 public class TreeTest {
+
+    public static TreeNode<Integer> takeInoutLevelWise(){
+        System.out.print("Enter the root value: ");
+        Scanner input = new Scanner(System.in);
+        int data = input.nextInt();
+
+        if(data == -1)
+            return null;
+
+        TreeNode<Integer> root = new TreeNode<>(data);
+        System.out.println();
+        Queue<TreeNode<Integer>> treeNodeQueue = new LinkedList<TreeNode<Integer>>();
+
+        treeNodeQueue.add(root);
+
+        while(!treeNodeQueue.isEmpty()){
+            TreeNode<Integer> curr = treeNodeQueue.poll();
+            System.out.println("Enter the number of children for "+curr.data);
+            int noOfChildren = input.nextInt();
+            if(noOfChildren == 0)
+                continue;
+            else{
+                for(int i=1;i<=noOfChildren;i++){
+                    System.out.print("Enter the data for child "+i+" of "+ curr.data+" : ");
+                    data = input.nextInt();
+                    TreeNode<Integer> child = new TreeNode<>(data);
+                    curr.children.add(child);
+                    treeNodeQueue.add(child);
+                    System.out.println();
+                }
+            }
+        }
+        return root;
+    }
 
     public static TreeNode<Integer> takeInputRecursive(){
         System.out.println("Enter the data of your node :");
@@ -57,7 +93,7 @@ public class TreeTest {
         node2.children.add(node4);
         node2.children.add(node5);
 
-        TreeNode<Integer> newRoot = takeInputRecursive();
+        TreeNode<Integer> newRoot = takeInoutLevelWise();
 
         printTreePreOrderBetter(newRoot);
 
