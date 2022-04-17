169123f87d07bf93429dab8a6c0eea15e8aaaba1
Nijin P S <nijingithub@gmail.com>
2022-04-17 21:57:55 +0530

    Add code to find LCA of BST

--- src/binarySearchTree_1/BSTtoLL.java
+++ src/binarySearchTree_1/BSTtoLL.java
@@ -1,47 +1,70 @@
 package binarySearchTree_1;
 
 /*
-Given a binary search tree and data of two nodes, find 'LCA' (Lowest Common Ancestor) of the given two nodes in the BST.
+Given a BST, convert it into a sorted linked list. You have to return the head of LL.
  */
 
 import binaryTree.BinaryTreeNode;
 
-public class BSTtoLL {
-    private static boolean find(BinaryTreeNode<Integer> root , int num){
-        if(root == null)
-            return false;
+import java.util.LinkedList;
 
-        if(root.data == num)
-            return true;
+class ReturnLL{
+    LinkedList<Integer> head;
+    LinkedList<Integer> tail;
 
-        return find(root.left, num) || find(root.right, num);
+    ReturnLL(LinkedList<Integer> head, LinkedList<Integer> tail){
+        this.head = head;
+        this.tail = tail;
     }
-
-    public static int getLCA(BinaryTreeNode<Integer> root, int a, int b) {
+}
+public class BSTtoLL {
+    public static ReturnLL constructLL(BinaryTreeNode<Integer> root)
+    {
         if(root == null)
-            return -1;
-        int max = Math.max(a, b);
-        int min = Math.min(a,b);
+            return null;
 
-        while(root!=null &&(root.data != min) && !(root.data>min && root.data<=max) ){
-            if(root.data < min)
-                root = root.right;
-            else if(root.data > max)
-                root = root.left;
+        if(root.left == null && root.right == null) //So it is a leaf
+        {
+            LinkedList<Integer> head = new LinkedList<>(root.data);
+            return new ReturnLL(head, head);
         }
 
+        LinkedList<Integer> rootLL = new LinkedList<>(root.data);
 
-        boolean findA = find(root, a);
-        boolean findB = find(root, b);
+        ReturnLL curr = new ReturnLL(null, null);
 
-        if(findA && findB)
-            return root.data;
+        ReturnLL left = constructLL(root.left);
+        ReturnLL right = constructLL(root.right);
 
-        else if(findA)
-            return a;
-        else if(findB)
-            return b;
-        else
-            return -1;
+
+        if(left == null){
+            curr.head = rootLL;
+        }
+
+        else{
+            left.tail.next = rootLL;
+            curr.head = left.head;
+        }
+
+        if(right == null){
+            curr.tail = rootLL;
+        }
+
+        else{
+            rootLL.next = right.head;
+            curr.tail = right.tail;
+        }
+
+        return curr;
+
+
+    }
+
+    public static LinkedList<Integer> constructLinkedList(BinaryTreeNode<Integer> root) {
+        if(root == null)
+            return null;
+        ReturnLL result = constructLL(root);
+        return result.head;
+
     }
 }
---
+++ src/binarySearchTree_1/LCAofBST.java
@@ -0,0 +1,47 @@
+package binarySearchTree_1;
+
+/*
+Given a binary search tree and data of two nodes, find 'LCA' (Lowest Common Ancestor) of the given two nodes in the BST.
+ */
+
+import binaryTree.BinaryTreeNode;
+
+public class LCAofBST {
+    private static boolean find(BinaryTreeNode<Integer> root , int num){
+        if(root == null)
+            return false;
+
+        if(root.data == num)
+            return true;
+
+        return find(root.left, num) || find(root.right, num);
+    }
+
+    public static int getLCA(BinaryTreeNode<Integer> root, int a, int b) {
+        if(root == null)
+            return -1;
+        int max = Math.max(a, b);
+        int min = Math.min(a,b);
+
+        while(root!=null &&(root.data != min) && !(root.data>min && root.data<=max) ){
+            if(root.data < min)
+                root = root.right;
+            else if(root.data > max)
+                root = root.left;
+        }
+
+
+        boolean findA = find(root, a);
+        boolean findB = find(root, b);
+
+        if(findA && findB)
+            return root.data;
+
+        else if(findA)
+            return a;
+        else if(findB)
+            return b;
+        else
+            return -1;
+    }
+}
