41600b103b6f6f6aba1451be89b982cc7b4b3c0b
Nijin P S <nijingithub@gmail.com>
2022-04-24 18:20:43 +0530

    Add code for BinaryTreeNode class

---
+++ src/binarySearchTree_2/BinaryTreeNode.java
@@ -0,0 +1,11 @@
+package binarySearchTree_2;
+
+public class BinaryTreeNode <T>{
+    BinaryTreeNode<T> left;
+    BinaryTreeNode<T> right;
+    T data;
+
+    BinaryTreeNode(T data){
+        this.data = data;
+    }
+}
